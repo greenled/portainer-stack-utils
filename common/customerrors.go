5dc5f43763dc1f7004ce683db1ce6fd85bd8ab6a
Juan Carlos Mejías Rodríguez <juan.mejias@reduc.edu.cu>
2019-08-02 13:14:23 -0400

    Remove unused CheckResponseForErrors error handling helper
    It's logic is now implemented into the Client

--- common/customerrors.go
+++ common/customerrors.go
@@ -1,13 +1,8 @@
 package common
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"log"
-	"net/http"
 )
 
 // CheckError checks if an error occurred (it's not nil)
@@ -15,24 +10,4 @@ func CheckError(err error) {
 	if err != nil {
 		log.Fatalln(fmt.Sprintf("Error: %s", err.Error()))
 	}
-}
-
-func CheckResponseForErrors(resp *http.Response) error {
-	if 300 <= resp.StatusCode {
-		// Guess it's a GenericError
-		respBody := GenericError{}
-		err := json.NewDecoder(resp.Body).Decode(&respBody)
-		if err != nil {
-			// It's not a GenericError
-			bodyBytes, err := ioutil.ReadAll(resp.Body)
-			defer resp.Body.Close()
-			if err != nil {
-				return err
-			}
-			resp.Body = ioutil.NopCloser(bytes.NewReader(bodyBytes))
-			return errors.New(string(bodyBytes))
-		}
-		return &respBody
-	}
-	return nil
 }
